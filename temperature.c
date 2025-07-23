d6cb90d8b3bd7071dbe54554545fcac063a06d80
Rohan <agarwalrohan169@gmail.com>
2025-07-23 10:07:45 +0530

    first commit

---
+++ temperature.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//print farenheit celsius table for fahr= 0,20,40...
+int main(){
+    float fahr, celsius;
+    float lower, upper, step;
+    lower =0;
+    upper =300;
+    step =20;
+    fahr =lower;
+    while(fahr<=upper){
+        celsius =5*(fahr-32)/9;
+        printf("%3.0f %6.1f\n",fahr,celsius);
+        fahr=fahr+step;
+    }
+    return 0;
+}
